5a844ba7e7638f424b944ed10c43248c73cc8b25
Cymentio <j.kondys@vp.pl>
2025-03-15 20:55:37 +0100

    add solver skeleton

--- main.go
+++ main.go
@@ -5,14 +5,19 @@ import (
 	"os"
 
 	"zad1/reader"
+	"zad1/solver"
 )
 
-// użycie: go run main.go <ścieżka_do_instancji>
+// użycie: go run main.go <ścieżka_do_instancji> [algorytm]
 func main() {
+	var algorithm string
 	args := os.Args[1:]
 	if len(args) == 0 {
-		fmt.Println("usage: go run main.go <path_to_instance>")
+		fmt.Println("usage: go run main.go <path_to_instance> [algorithm]")
 		return
+	}
+	if len(args) > 1 {
+		algorithm = args[1]
 	}
 	nodes, headers, err := reader.ReadInstance(args[0])
 	if err != nil {
@@ -21,4 +26,11 @@ func main() {
 	}
 	fmt.Println(nodes)
 	fmt.Println(headers)
+
+	order, err := solver.Solve(nodes, algorithm)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(order)
 }
